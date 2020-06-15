8f3df69853bef9cb4eac8c9066c74809a9cd68ec
allezz <aerbay@gmail.com>
2020-06-15 20:29:36 +0200

    Add react-router-dom,Add route to app.jsx
    Co-authored-by: PaulineBA <barnades.pauline@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import AllQuests from "./components/AllQuests";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={AllQuests}></Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
