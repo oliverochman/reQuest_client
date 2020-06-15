8fb0bfeaf447105ba80be7ac2d0871f9819446b4
allezz <aerbay@gmail.com>
2020-06-15 18:56:52 +0200

    Add components,Add axios

---
+++ src/components/AllQuests.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllQuests = () => {
+  return <div></div>;
+};
+
+export default AllQuests;
---
+++ src/components/QuestCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestCard = () => {
+  return <div></div>;
+};
+
+export default QuestCard;
