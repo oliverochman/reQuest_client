4280b22e78ad0260b3853943829b4fe960b55046
allezz <aerbay@gmail.com>
2020-06-16 00:42:07 +0200

    Add styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import AllQuests from "./components/AllQuests";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      {/* <Header></Header> */}
+      <Header />
       <Switch>
         <Route exact path="/" component={AllQuests}></Route>
       </Switch>
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div id="header">
+        <p style={{ float: "left" }} id="hfirst">
+          re
+        </p>
+        <p style={{ float: "left" }} id="hsecond">
+          Quest
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
