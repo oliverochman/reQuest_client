64b1b8183675e019cad0a1c6cffa7c59f2856b40
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-17 21:40:21 +0200

    removing thunk which apparently was unnecessary
    Co-authored-by: Ali <aerbay@gmail.com>

--- src/state/store/configureStore.js
+++ src/state/store/configureStore.js
@@ -1,8 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
-import thunk from "redux-thunk"
 
 const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(rootReducer);
 };
 export default configureStore;
