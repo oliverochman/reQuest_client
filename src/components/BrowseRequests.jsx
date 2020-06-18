57bdd40717f6d072755d350ce8302de04b0e09c8
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-06-18 16:52:58 +0200

    adds styling to match lofi and offer nice responsiveness

--- src/components/BrowseRequests.jsx
+++ src/components/BrowseRequests.jsx
@@ -6,11 +6,11 @@ import SpecificRequest from './SpecificRequest'
 const BrowseRequests = () => {
   return (
     <>
-      <div id="page-container" style={{ padding: "50px", paddingTop: "40px", width: "100vw"}}>
-      <h1 style={{position: "absolute", marginTop: "-90px", color: "whitesmoke", fontSize: "300%"}} >Browse reQuests</h1>
-        <Grid>
-          <Grid.Column width={2}>aa</Grid.Column>
-          <Grid.Column width={9}><AllRequests></AllRequests></Grid.Column>
+      <div id="page-container" style={{ padding: "50px", paddingTop: "20px", width: "100vw"}}>
+      <h1 style={{position: "absolute", marginTop: "-80px", color: "whitesmoke", fontSize: "300%"}} >Browse reQuests</h1>
+        <Grid style={{width: "100%"}}>
+          <Grid.Column width={2}></Grid.Column>
+          <Grid.Column width={9}><AllRequests /></Grid.Column>
           <Grid.Column width={5}><SpecificRequest /></Grid.Column>
         </Grid>
       </div>
--- src/components/SpecificRequest.jsx
+++ src/components/SpecificRequest.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Divider } from 'semantic-ui-react'
 
 const SpecificRequest = () => {
   const selectedRequest = useSelector(state => state.requests.selectedRequest)
@@ -27,31 +27,36 @@ const SpecificRequest = () => {
       }
       return (
         <div id="selected-request">
-          <h3 id="selected-title">{selectedRequest.title}</h3>
-          <p id="selected-requester">{selectedRequest.requester}</p>
-          <p id="selected-description">{selectedRequest.description}</p>
-          <div id="selected-reward">{selectedRequest.reward}</div>
-          <Button id="contact-button" disabled={disableButton}>Contact</Button>
+          <h2 id="selected-title">{selectedRequest.title}</h2>
+          <span id="selected-requester">{selectedRequest.requester}</span>
+          <Divider />
+          <div id="description-wrapper">
+            <p id="selected-description">{selectedRequest.description}</p>
+          </div>
+          <div id="selected-reward">{selectedRequest.reward}p</div>
+          <Button id="contact-button" disabled={disableButton}>contact</Button>
           {message && <span id="selected-message">{message}</span>}
         </div>
       )
     } else {
       return (
         <div id="about-request">
-          <h3>The big idea - You get what you give</h3>
-          <p>reQuest is an app for people helping each other with an in-app currency (karma points) as reward.</p>
-          <p>So in order to have people help you with reQuests, you need to help others with their Quests.</p>
-          <p>
-            You can post a reQuest for a certain task to be done.
-            People  can respond to reQuests and say that they can do it.   You both agree on the reward.
-          </p>
-          <p>
-            When the task is done you pay the person that performed the task with in-app currency (karma points).
-            Then you can rate each other, so that trust-worthy people will get a higher rating.
-          </p>
-          <p>
-            In order to  get started , you will need to create an account to post and respond to reQuests.
-          </p>
+          <div>
+            <h3>The big idea - You get what you give</h3>
+            <p>reQuest is an app for people helping each other with an in-app currency (karma points) as reward.</p>
+            <p>So in order to have people help you with reQuests, you need to help others with their Quests.</p>
+            <p>
+              You can post a reQuest for a certain task to be done.
+              People  can respond to reQuests and say that they can do it.   You both agree on the reward.
+            </p>
+            <p>
+              When the task is done you pay the person that performed the task with in-app currency (karma points).
+              Then you can rate each other, so that trust-worthy people will get a higher rating.
+            </p>
+            <p>
+              In order to  get started , you will need to create an account to post and respond to reQuests.
+            </p>
+          </div>
         </div>
       )
     }
