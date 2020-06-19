71081a4fb200fb211e659f826fff2e14e1f2c4f0
allezz <aerbay@gmail.com>
2020-06-19 12:53:37 +0200

    Divide test examples

--- cypress/integration/requesterCanMarkARequestAsCompleted.feature.js
+++ cypress/integration/requesterCanMarkARequestAsCompleted.feature.js
@@ -22,20 +22,27 @@ describe("Requested can mark a request as completed", () => {
     cy.get("#myrequest-home-link").click();
     cy.get("#requests-link").click();
   });
-  it("should have a quest completed button and can complete request", () => {
-    cy.server();
-    cy.route({
-      method: "GET",
-      url: "**/my_requests/requests",
-      response: "fixture:active_specific_request_with_offer.json",
-      headers: {
-        uid: "me@mail.com",
-      },
+  describe("should have a quest completed button and can complete request", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "**/my_requests/requests",
+        response: "fixture:active_specific_request_with_offer.json",
+        headers: {
+          uid: "me@mail.com",
+        },
+      });
     });
-    cy.get("#active").click();
-    cy.get("#my-list").should("contain", "Change to winter tires");
-    cy.get("#quest-completed").should("exist");
-    cy.get("#quest-completed").click({ force: true });
-    cy.get("#message").should("contain", "Request completed");
+    it("have complete button", () => {
+      cy.get("#active").click();
+      cy.get("#my-list").should("contain", "Change to winter tires");
+      cy.get("#quest-completed").should("exist");
+    });
+    it("can complete request", () => {
+      cy.get("#active").click();
+      cy.get("#quest-completed").click({ force: true });
+      cy.get("#message").should("contain", "Request completed");
+    });
   });
 });
