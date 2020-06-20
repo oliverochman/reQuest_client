8c3a28a2a0aa8fa2c325d4ed0a8827d7aa4551b7
japh <jenny@performanceit.se>
2020-06-20 13:21:51 +0200

    update declined path

--- cypress/integration/userCanAcceptOrDeclineAHelpOffer.feature.js
+++ cypress/integration/userCanAcceptOrDeclineAHelpOffer.feature.js
@@ -81,26 +81,50 @@ describe("User can", () => {
     });
   });
 
-  xdescribe("successfully decline a help offer by clicking 'Decline'", () => {
+  describe("successfully decline a help offer by clicking 'Decline'", () => {
     beforeEach(() => {
-      cy.get("#request-1").within(() => {
-        cy.get("#offer-1").within(() => {
-          cy.get("button#accept").contains("Decline").click();
-        });
+      cy.route({
+        method: "GET",
+        url: "**/my_requests/requests",
+        response: "fixture:list_of_my_requests.json",
+        headers: {
+          uid: "me@mail.com",
+        },
       });
+      cy.route({
+        method: "GET",
+        url: "**/my_request/requests/1",
+        response: "fixture:view_specific_request_with_pending_offers.json",
+        headers: {
+          uid: "me@mail.com",
+        },
+      });
+      cy.route({
+        method: "PUT",
+        url: "**/offers",
+        response: "fixture:putOffer_declined.json",
+        headers: {
+          uid: "me@mail.com",
+        },
+      });
+      cy.login();
+      cy.get("#myrequest-home-link").click();
+      cy.get("#requests-link").click();
+      cy.get("#request-1").click();
+      cy.get("#helper-message").should("not.be.visible");
+      cy.get("#offer-2").within(() => {
+        cy.get(".helper-email-2").should("be.visible").click();
+      });
+      // cy.get(".helper-email-2").click();
+      cy.get("button#declined").should("be.visible");
+      cy.wait(1000);
     });
 
-    it("and sees a success message", () => {
-      cy.get("#decline-message").should(
-        "contain",
+    it("and sees a decline success message", () => {
+      cy.get("button#declined").contains("Decline").click();
+      cy.get("p#status-message").contains(
         "You declined help from helper@mail.com"
       );
-    });
-
-    it("and the declined offer disappears", () => {
-      cy.get("#request-1").within(() => {
-        cy.get("#offer-1").should("not.exist");
-      });
     });
   });
 });
