e3e5d414978e9abd4171f9cb7ac72f1929c19e5d
pauline_barnades <barnades.pauline@gmail.com>
2020-06-21 09:57:22 +0200

    Adding test

---
+++ cypress/integration/VisitorCanBrowseByCategory.feature.js
@@ -0,0 +1,23 @@
+describe("Visitor can browse by category", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit("/")
+  });
+
+  it("and they should all be visible", () => {
+    cy.get("#Home").should("be visible")
+    cy.get("#Education").should("be visible")
+    cy.get("#IT").should("be visible")
+    cy.get("#Vehicles").should("be visible")
+    cy.get("#Others").should("be visible")
+  });
+
+  it("and they should be clickable", () => {
+    cy.get("#Home").click()
+    cy.get("#Education").click()
+  });
+
+  it("only existing categories", () => {
+    cy.get("cleaning").should("not be visible")
+  })
+})
