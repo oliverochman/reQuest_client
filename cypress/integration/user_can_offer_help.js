b9a7e15d3db8747a004d5f1f49d140cdfa534243
japh <jenny@performanceit.se>
2020-06-21 13:44:54 +0200

    update cypress test

--- cypress/integration/user_can_offer_help.js
+++ cypress/integration/user_can_offer_help.js
@@ -1,27 +1,36 @@
 describe("User can offer help", () => {
   beforeEach(() => {
     cy.server();
-    cy.login();
-    cy.stubMainLoggedIn();
     cy.route({
       method: "POST",
-      url: "**/my_request/requests",
-      response: "fixture:requests/post_new_request.json",
+      url: "**/offers",
+      response: { message: "Your offer has been sent!" },
       headers: {
         uid: "user@mail.com",
       },
     });
+    cy.login();
+    cy.stubMainLoggedIn();
   });
 
-  describe("send message to reQuester", () => {
-    it("helper can pick a reQuest", () => {
+  describe("sucessfully ", () => {
+    beforeEach(() => {
+      cy.StubRequestPendingOffer();
       cy.get("#request-1").within(() => {
         cy.get(".header").should("contain", "Change to winter tires").click();
       });
       cy.get("#selected-title").contains("Change to winter tires");
       cy.get("#contact-button").click();
-      cy.get("#request-2").should("not.be.visible");
-      cy.get("#message-area").should("be.visible");
+      cy.get("#offerMessage")
+        .should("be.visible")
+        .type(
+          "Im so handy. I have been handy my hole carrerier. I can do anything with silver tejp. Ready when you have time. // MacGyver"
+        );
+    });
+    it(" message is shown", () => {
+      cy.get("button#message-send-btn").contains("Send").click();
+      cy.wait(3000);
+      cy.get("p#message").should("contain", "Your offer has been sent!");
     });
   });
 });
