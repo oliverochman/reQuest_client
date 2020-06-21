3a29f782d295eb4c3f41b722d7bb637615adf707
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-21 19:23:26 +0200

    extending test with some checks for clicking around

--- cypress/integration/userCanViewTheirActiveRequests.feature.js
+++ cypress/integration/userCanViewTheirActiveRequests.feature.js
@@ -17,6 +17,14 @@ describe("User can view their active reQuests", () => {
         uid: "me@mail.com",
       },
     });
+    cy.route({
+      method: "GET",
+      url: "**/my_request/requests/6",
+      response: "fixture:requests/another_active_specific_request_with_offer.json",
+      headers: {
+        uid: "me@mail.com",
+      },
+    })
     cy.login();
     cy.get("#myrequest-home-link").click();
     cy.get("#requests-link").click();
@@ -48,4 +56,42 @@ describe("User can view their active reQuests", () => {
       "I can help you with this, and I'm probably the first to offer my help"
     );
   });
+
+  it("when a second reQuest is clicked, it's expanded instead of the first", () => {
+    cy.get("#active-link").click();
+    cy.get("#request-5").click();
+    cy.wait(800)
+    cy.get("#request-6").click();
+    cy.get("#request-description-6").should("be.visible");
+    cy.get("#offers > .cards > .ui > .content > .meta").should(
+      "contain",
+      "offerer@mail.com"
+    );
+    cy.get("#offer-message").should(
+      "contain",
+      "I can copy the fixtures, I'm fast"
+    );
+  })
+
+  it("clicking a reQuest a second time makes it collapse", () => {
+    cy.get("#active-link").click();
+    cy.get("#request-5").click();
+    cy.wait(800)
+    cy.get("#request-5").click();
+    cy.get("#request-description-6").should("not.be.visible");
+  })
+
+  it("when navigating to another tab, the active reQuest and message disappears", () => {
+    cy.get("#active-link").click();
+    cy.get("#request-5").click();
+    cy.get("#request-description-5").should("be.visible");
+    cy.get("#offers > .cards > .ui > .content > .meta").should(
+      "contain",
+      "helper@mail.com"
+    );
+    cy.get("#offer-message").should("be.visible");
+    cy.get("#pending-link").click();
+    cy.get("#request-description-5").should("not.be.visible");
+    cy.get("#offer-message").should("not.be.visible");
+  })
 });
