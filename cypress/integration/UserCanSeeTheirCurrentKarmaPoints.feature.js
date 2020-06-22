0b84901639256f253ce696c981f594bb958ffa33
designerofthing <designerofthings@gmail.com>
2020-06-22 13:02:14 +0200

    adds test for change in points, changes color to white, reinstates line in loginform

--- cypress/integration/UserCanSeeTheirCurrentKarmaPoints.feature.js
+++ cypress/integration/UserCanSeeTheirCurrentKarmaPoints.feature.js
@@ -2,11 +2,36 @@ describe("User can see their current karma points", () => {
   beforeEach(() => {
     cy.server();
     cy.login();
-  });
-  it("should display users current karma points in header", () => {
     cy.get("#myrequest-home-link").click();
     cy.get("#requests-link").click();
+  
+  });
+  it("should display users current karma points in header", () => {
     cy.get("#points-display").should("exist");
     cy.get("#karma-points-amount").should("contain", "100p");
+  });
+  it("should display a reduced amount after creating a request", () => {
+    cy.get("#create-request-link").click();
+    cy.get("#title").type("Fix my bike");
+    cy.get("#description").type("I cant ride my bike, HILFE, hilfe, pronto!");
+    cy.get("#reward").type("50");
+    cy.route({
+      method: "GET",
+      url: "**/karma_points*",
+      response: { karma_points: 50 },
+      headers: {
+        uid: "user@mail.com",
+      },
+    });
+    cy.route({
+      method: "POST",
+      url: "**/requests",
+      response: "fixture:post_new_request.json",
+      headers: {
+        uid: "user@mail.com",
+      },
+    });
+    cy.get("#submit-btn").contains("Submit").click();
+    cy.get("#karma-points-amount").should("contain", "50p");
   });
 });
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,8 +10,8 @@ const LoginForm = () => {
   const history = useHistory();
 
   useEffect(() => {
-    dispatch({type: "RESET_ACTIVE_PAGE"})
-  }, [dispatch])
+    dispatch({ type: "RESET_ACTIVE_PAGE" });
+  }, [dispatch]);
 
   const login = async (e) => {
     try {
@@ -25,14 +25,15 @@ const LoginForm = () => {
           payload: {
             authenticated: response.success,
             uid: response.data.uid,
-          }, 
+          },
         });
         dispatch({
-            type: "GET_KARMA",
-            payload: {
-              karma: response.data.karma_points,
-            },
+          type: "GET_KARMA",
+          payload: {
+            karma: response.data.karma_points,
+          },
         });
+        history.push("/");
       }
     } catch (error) {
       setErrorMessage(error.response.data.errors[0]);
