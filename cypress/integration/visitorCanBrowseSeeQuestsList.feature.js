0a24971e2c730ee6929299965b509babb3151ba1
allezz <aerbay@gmail.com>
2020-06-23 16:55:34 +0200

    Add stub location for view list test
    Co-authored-by: PaulineBA <barnades.pauline@gmail.com>

--- cypress/integration/visitorCanBrowseSeeQuestsList.feature.js
+++ cypress/integration/visitorCanBrowseSeeQuestsList.feature.js
@@ -1,9 +1,11 @@
+const stubLocation = require("../support/stubLocation");
+
 describe("Visitor can view all listed requests", () => {
   beforeEach(() => {
     cy.stubMain();
   });
   it("reQuests are shown", () => {
-    cy.visit("/");
+    cy.visit("/", stubLocation({ latitude: 57.71, longitude: 11.97 }));
     cy.get("#request-1").within(() => {
       cy.get("div.header").should("contain", "Change to winter tires");
     });
