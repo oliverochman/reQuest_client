3194a290118e5a12dd76bff85fe9d147a1b67c8b
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-25 13:01:29 +0200

    Adjusting app description
    Co-authored-by: Steve <designerofthings@gmail.com>

--- src/components/AboutReQuest.jsx
+++ src/components/AboutReQuest.jsx
@@ -5,28 +5,24 @@ export const AboutReQuest = () => {
   return (
     <div id="about-request">
       <div>
-        <h3>The big idea - You get what you give</h3>
-        <p>
+        <h3 id='about-title'>The big idea</h3> <br/> <h3 id='about-byline'>You get what you give</h3>
+        <p id='about-body'>
           reQuest is an app for people helping each other with an in-app
-          currency (karma points) as reward.
-        </p>
-        <p>
+          currency (karma points) as reward. <br/> <br/>
+       
           So in order to have people help you with reQuests, you need to help
-          others with their Quests.
-        </p>
-        <p>
+          others with their reQuests, which becomes your Quests.<br/> <br/>
+        
           You can post a reQuest for a certain task to be done. People can
           respond to reQuests and say that they can do it. You both agree on the
-          reward.
-        </p>
-        <p>
+          reward. <br/> <br/>
+        
           When the task is done you pay the person that performed the task with
           in-app currency (karma points). Then you can rate each other, so that
-          trust-worthy people will get a higher rating.
-        </p>
-        <p>
-          In order to get started , you will need to create an account to post
-          and respond to reQuests.
+          trust-worthy people will get a higher rating. <br/> <br/>
+       
+          In order to get started, you will need to create an account to post
+          and respond to reQuests. <br/>
         </p>
       </div>
     </div>
@@ -37,7 +33,7 @@ export const SelectedRequest = (props) => {
   return (
     <div id="selected-request">
       <h3 id="selected-title">{props.selectedRequest.title}</h3>
-      <span id="selected-requester">{props.selectedRequest.requester}</span>
+      <h5 id="selected-requester">{props.selectedRequest.requester}</h5>
       <Divider />
       <div id="description-wrapper">
         <p id="selected-description">{props.selectedRequest.description}</p>
